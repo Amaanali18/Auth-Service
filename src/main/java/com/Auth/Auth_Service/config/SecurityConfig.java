5ab358fa692b3fe803970835b824f3cd703ab440
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-01-14 15:24:11 +0530

    added security config

--- src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
+++ src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
@@ -14,10 +14,10 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         User.UserBuilder userBuilder = User.builder();
-        UserDetails user1 = userBuilder.username("admin").password("admin").build();
+//        UserDetails user1 = userBuilder.username("admin").password("admin").build();
         UserDetails user2 = userBuilder.username("amaanali").password("password").build();
         UserDetails user3 = userBuilder.username("manager").password("secure-password").build();
         UserDetails user4 = userBuilder.username("administrator").password("hashed-secure-password").build();
-        return  new InMemoryUserDetailsManager(user1,user2,user3,user4);
+        return  new InMemoryUserDetailsManager(user2,user3,user4);
     }
 }
