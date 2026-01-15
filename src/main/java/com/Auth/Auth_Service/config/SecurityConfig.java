0d3b3c81612be58f67a15bf864fe4711a191b184
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-01-15 21:26:03 +0530

    added security config

--- src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
+++ src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
@@ -15,9 +15,9 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         User.UserBuilder userBuilder = User.builder();
 //        UserDetails user1 = userBuilder.username("admin").password("admin").build();
-        UserDetails user2 = userBuilder.username("amaanali").password("password").build();
-        UserDetails user3 = userBuilder.username("manager").password("secure-password").build();
-        UserDetails user4 = userBuilder.username("administrator").password("hashed-secure-password").build();
-        return  new InMemoryUserDetailsManager(user2,user3,user4);
+        UserDetails userME = userBuilder.username("amaanali").password("password").build();
+        UserDetails userMng = userBuilder.username("manager").password("secure-password").build();
+        UserDetails userAdm = userBuilder.username("administrator").password("hashed-secure-password").build();
+        return  new InMemoryUserDetailsManager(userME,userMng,userAdm);
     }
 }
