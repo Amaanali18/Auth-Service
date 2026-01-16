971b80e7a39200116bb23201274ea0d222988acd
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-01-16 21:26:30 +0530

    updated security config

--- src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
+++ src/main/java/com/Auth/Auth_Service/config/SecurityConfig.java
@@ -11,13 +11,13 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        User.UserBuilder userBuilder = User.builder();
-//        UserDetails user1 = userBuilder.username("admin").password("admin").build();
-        UserDetails userME = userBuilder.username("amaanali").password("password").build();
-        UserDetails userMng = userBuilder.username("manager").password("secure-password").build();
-        UserDetails userAdm = userBuilder.username("administrator").password("hashed-secure-password").build();
-        return  new InMemoryUserDetailsManager(userME,userMng,userAdm);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        User.UserBuilder userBuilder = User.builder();
+////        UserDetails user1 = userBuilder.username("admin").password("admin").build();
+//        UserDetails userME = userBuilder.username("amaanali").password("password").build();
+//        UserDetails userMng = userBuilder.username("manager").password("secure-password").build();
+//        UserDetails userAdm = userBuilder.username("administrator").password("hashed-secure-password").build();
+//        return  new InMemoryUserDetailsManager(userME,userMng,userAdm);
+//    }
 }
